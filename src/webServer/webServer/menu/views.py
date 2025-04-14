c183ddfe9239185824122c61dd044382e338578e
Jaime Alvarez Diaz <alvarez.diaz.jaime1@gmail.com>
2025-04-14 21:32:35 +0200

    Create menu template

--- src/webServer/webServer/menu/views.py
+++ src/webServer/webServer/menu/views.py
@@ -1,9 +1,23 @@
+from dataclasses import dataclass
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def menu(request) -> HttpResponse:
-    return HttpResponse('Web server menu')
+    @dataclass
+    class App:
+        name: str
+        url: str
+
+    appsList: list[App] = [
+        App('Weather', '/weather'),
+        App('Clock', '/clock'),
+    ]
+    context: dict = {
+        'appsList': appsList
+    }
+    return render(request, 'menu/index.html', context)
 
 def info(request) -> HttpResponse:
     return HttpResponse('Web server menu info')
